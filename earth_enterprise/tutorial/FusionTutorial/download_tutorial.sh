1c63d429aa42422d6fd80ba878aec0ed1f8462d1
CV. HARIS ATATAMA <52840328+HARISATATAMa@users.noreply.github.com>
2021-02-27 19:55:12 +0700

    Initialize release_5.2.1

---
+++ earth_enterprise/tutorial/FusionTutorial/download_tutorial.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TMPFILE="/tmp/FusionTutorial-Full.tar.gz"
+SCRIPTDIR=`dirname "$0"`
+SELFNAME=`basename "$0"`
+
+if [ $(id -u) != 0 ] ; then
+    echo "$SELFNAME must be run as root"
+    exit 1;
+fi
+
+if [ ! -d "$SCRIPTDIR/Imagery" ]; then
+	wget "http://data.opengee.org/FusionTutorial-Full.tar.gz" -O "$TMPFILE"
+	tar -xvzf "$TMPFILE" -C "$SCRIPTDIR"
+	rm -f "$TMPFILE"
+	chown -R gefusionuser:gegroup "$SCRIPTDIR"
+fi
